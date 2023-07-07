705ac1af3da86172ec6cd932879cfae1ae112a35
Karan <karan.singh@squapl.com>
2023-07-08 00:11:31 +0530

    v1.0

--- school-management-system/src/main/java/com/onlineschool/schoolmanagementsystem/SchoolManagementSystemApplication.java
+++ school-management-system/src/main/java/com/onlineschool/schoolmanagementsystem/SchoolManagementSystemApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolManagementSystemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolManagementSystemApplication.class, args);
+		System.out.println("hello world");
 	}
-
 }
